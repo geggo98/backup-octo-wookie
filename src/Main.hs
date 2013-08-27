996e5e6841078ca9d14757e58e6681576fe12dda
Stefan Schwetschke <Stefan.Schwetschke@fernuni-hagen.de>
2013-08-27 15:28:44 +0000

    Problem 11

--- src/Main.hs
+++ src/Main.hs
@@ -22,7 +22,11 @@ module Main where
 import Data.Array.Unboxed
 import Data.Foldable (maximumBy, toList)
 import qualified Data.Sequence as Seq
- 
+import Data.List.Split (splitOn)
+--import qualified Data.Array.Repa as R -- Currently no repa on FPcomplete available
+import qualified Data.Vector as V 
+import Data.List (nub)
+
 -- Problem 1
 problem1 = foldl (+) 0 [x | x <- [1 .. 999], x `mod` 3 == 0 || x `mod` 5 == 0 ] 
 
@@ -112,16 +116,57 @@ problem8 = foldr (max) 0 $ map (foldr (*) 1) $ map (take 5) $ paramorphism phi [
 
 -- Problem 9
 
-problem9 = [(a,b,c a b, round(a*b*c a b)) | a <- [1..1000], b <- [1..1000], sqr(a)+sqr(b)==sqr(1000-a-b)]
+problem9 = [(a,b,dist a b, round(a*b*dist a b)) | a <- [1..1000], b <- [1..1000], sqr(a)+sqr(b)==sqr(1000-a-b)]
     where
-        c a b = sqrt(sqr(a)+sqr(b)) 
+        dist a b = sqrt(sqr(a)+sqr(b)) 
         sqr x = x*x
 
 -- Problem10
 
 problem10 = foldr (+) 0 $ primesToA 2000000
 
+-- Problem11
+
+input11raw="\
+\08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08|\
+\49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00|\
+\81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65|\
+\52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91|\
+\22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80|\
+\24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50|\
+\32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70|\
+\67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21|\
+\24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72|\
+\21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95|\
+\78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92|\
+\16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57|\
+\86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58|\
+\19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40|\
+\04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66|\
+\88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69|\
+\04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36|\
+\20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16|\
+\20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54|\
+\01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48"
+
+
+
+problem11 = maximum $ map (product) $ map (map (\(x,y) -> inputMatrix V.! y V.! x)) indexes
+    where
+        d = V.length inputMatrix
+        inputList = map (map (\s -> read s :: Int)) $ map (splitOn " ") $ splitOn "|" input11raw
+        inputMatrix = V.fromList $ map (V.fromList) inputList    
+        direction = [(dx,dy) | dx <- [-1,0,1], dy <- [-1,0,1] ]
+        step (x,y) (dx,dy) = (x+dx, y+dy)
+        allDeltas = [scanl (step) (0,0) [d1, d2, d3, d4] | d1 <- direction, d2 <- direction, d3 <- direction, d4 <- direction]
+        uniqueDeltas = nub $ filter (\l -> length l == 4) $ map (nub) $ filter (all (\(x,y) -> x>=0 && y>=0)) allDeltas
+        -- The question in deed only wants the direct lines: left, right, and the two diagonals. This malkes things much easier
+        directDeltas=[[(0,0),(0,1),(0,2),(0,3)],[(0,0),(1,0),(2,0),(3,0)],[(0,0),(1,1),(2,2),(3,3)], [(3,0),(2,1),(1,2),(0,3)]]
+        allIndexes = [ map (\(dx,dy) -> (x+dx, y+dy)) delta | x <- [0..d-1], y <- [0..d-1], delta <- directDeltas]
+        indexes = filter (all (\(x,y) -> x<d && y<d)) allIndexes
+
+
 -- | The main entry point.
 main :: IO ()
-main = putStrLn $ show $ problem10
+main = putStrLn $ show $ problem11
 
