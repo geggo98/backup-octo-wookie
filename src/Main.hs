46d15da2867f6f5a22cdf56067c82de97a658e01
Stefan Schwetschke <stefan@schwetschke.de>
2014-01-25 23:48:59 +0000

    Added maximum path sums (Problem 17 and 67)

--- src/Main.hs
+++ src/Main.hs
@@ -45,8 +45,11 @@ import Data.Array.Base
 import Control.Monad.ST
 import Data.Bits
 
+import Data.Char (digitToInt)
+
 import Numeric.SpecFunctions (factorial)
 
+import System.IO.Unsafe
 
 -- Problem 1
 problem1 = foldl (+) 0 [x | x <- [1 .. 999], x `mod` 3 == 0 || x `mod` 5 == 0 ] 
@@ -280,10 +283,52 @@ problem15 = map numMoves [1,2,3,20]
         factorialInt i = product [0..i]
 
 -- Problem 16
+problem16 = sum $ map digitToInt $ show $ foldl (*) 1 $ replicate 10000 2
 
+-- Problem 17
+input17raw="75|\
+\25 34|\
+\17 47 82|\
+\18 35 87 10|\
+\20 04 82 47 65|\
+\19 01 23 75 03 34|\
+\88 02 77 73 07 63 67|\
+\99 65 04 28 06 16 70 92|\
+\41 41 26 56 83 40 80 70 33|\
+\41 48 72 33 47 32 37 16 94 29|\
+\53 71 44 65 25 43 91 52 97 51 14|\
+\70 11 33 28 77 73 17 78 39 68 17 57|\
+\91 71 52 38 17 14 91 43 58 50 27 29 48|\
+\63 66 04 68 89 53 67 30 73 16 69 87 40 31|\
+\04 62 98 27 23 09 70 28 73 93 38 53 60 04 23"
+
+input17rawSmall = "3|\
+\7 4|\
+\2 4 6|\
+\8 5 9 3"
+
+parseMatrix lineSplit input= map (map (\s -> read s :: Int)) $ map (splitOn " ") $ splitOn lineSplit input
+
+input17 = parseMatrix "|" input17raw
+
+maxPathSum m = maximum $ foldl maxLine (head m) (tail m)
+    where
+        maxLine a b = map maxSum $ zip3 (0:a) (a ++ [0]) b
+        maxSum (left, right, down) = maximum [left+down, right+down]
+
+problem17 = maxPathSum input17
+
+-- Problem 67
+input67 = unsafePerformIO readTriangle
+    where
+        readTriangle = do
+            input <- readFile "triangle.txt"
+            return (parseMatrix "\n" input)
+
+problem67 = maxPathSum input67
 
 -- | The main entry point.
 main :: IO ()
-main = putStrLn $ show $ problem15
+main = putStrLn $ show $ problem67
 
 
